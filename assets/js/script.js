e1d3ec402e0441cd30505c5503ee7c93134f5895
Tanja Franke <wwicxcut@students.codeinstitute.net>
2024-10-30 18:38:42 +0000

    Set structure of game functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,27 @@
+function runGame(){
 
+}
+function checkAnswer(){
+
+}
+function CalculateCorrectAnswers(){
+
+}
+function incrementScore(){
+
+}
+function incrementWrongAnswer(){
+
+}
+function displayAdditionQuestion(){
+
+}
+function displaySubtractQuestion(){
+
+}
+function displayMultiplyQuestion(){
+
+}
+function displayDivisionQuestion(){
+    
+}
