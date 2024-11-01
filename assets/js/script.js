e0909c870875c3fd4a72cab84179e9fb6c301db2
Tanja Franke <wwicxcut@students.codeinstitute.net>
2024-11-01 10:42:59 +0000

    Display addition question to user

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,11 @@
 // Wait for the DOM to finish loading before running the game
 // Get the button elements and add event listeners to them
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     let buttons = document.getElementsByTagName("button");
 
     for (let button of buttons) {
-        button.addEventListener("click", function() {
+        button.addEventListener("click", function () {
             if (this.getAttribute("data-type") === "submit") {
                 alert("You clicked Submit!");
             } else {
@@ -25,29 +25,45 @@ function runGame() {
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
 
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
 }
 
-function checkAnswer(){
+function checkAnswer() {
 
 }
-function CalculateCorrectAnswers(){
+
+function CalculateCorrectAnswers() {
 
 }
-function incrementScore(){
+
+function incrementScore() {
 
 }
-function incrementWrongAnswer(){
+
+function incrementWrongAnswer() {
 
 }
-function displayAdditionQuestion(){
+
+function displayAdditionQuestion(operand1, operand2) {
+
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
+}
+
+function displaySubtractQuestion() {
 
 }
-function displaySubtractQuestion(){
+
+function displayMultiplyQuestion() {
 
 }
-function displayMultiplyQuestion(){
 
-}
-function displayDivisionQuestion(){
-    
+function displayDivisionQuestion() {
+
 }
