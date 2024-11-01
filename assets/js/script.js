a12cbfb8e24317808ba1bc35f952af551471de58
Tanja Franke <wwicxcut@students.codeinstitute.net>
2024-11-01 10:51:11 +0000

    Add parameter to the runGame function

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         })
     }
@@ -20,7 +20,7 @@ document.addEventListener("DOMContentLoaded", function () {
  * The main game "loop", called when the script is first loaded
  * and after the user's answer has been processed
  */
-function runGame() {
+function runGame(gameType) {
 
     // Creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
